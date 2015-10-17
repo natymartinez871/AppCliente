f62b15e904d11142f58c79b07ce35e73954de74b
Natasha Martínez <natymartinez871@gmail.com>
2015-10-17 00:33:16 -0430

    17/10/2015 - C/P

--- src/com/ayalamart/appcliente/Act_Menu.java
+++ src/com/ayalamart/appcliente/Act_Menu.java
@@ -18,37 +18,40 @@ import com.ayalamart.modelo.Plato;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class Act_Menu extends Activity{
-	
+
 	private static final String TAG = Act_Menu.class.getSimpleName(); 
-	
-	private static String Url = "http://api.androidhive.info/json/movies.json"; 
+
 	private static String URL_Platos = "http://10.10.0.99:8080/Restaurante/rest/plato/getPlatosAll"; 
 	private static String URL_Platos_N = "http://10.0.2.2:8080/Restaurante/rest/plato/getPlatosAll"; 
-	 private ProgressDialog pDialog;
-	 private List<Plato> listaPlato = new ArrayList<Plato>(); 
-	 private CustomListAdapter adapter; 
-	 private ListView listView; 
-	 private int j; 
-	 private TextView total; 
-	 
-	 @Override
+	private ProgressDialog pDialog;
+	private List<Plato> listaPlato = new ArrayList<Plato>(); 
+	private CustomListAdapter adapter; 
+	private ListView listView; 
+	private int j; 
+	private TextView total; 
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_act__menu);
-		
+
 		listView = (ListView)findViewById(R.id.LV_menu); 
 		adapter = new CustomListAdapter(this, listaPlato, new BtnClickListener() {
-			
+
 			@Override
 			public void onBtnClick(int position) {
-				
-			/*	Plato plato = new Plato(); 
+
+				/*	Plato plato = new Plato(); 
 				j = plato.getPrecio(); 
 				String k = total.getText().toString(); 
 				if (k != "00") {
@@ -61,19 +64,20 @@ public class Act_Menu extends Activity{
 				}*/
 			}
 		}); 
+
+
 		listView.setAdapter(adapter);
-		
 		pDialog = new ProgressDialog(this); 
 		pDialog.setMessage("Cargando...");
 		pDialog.show(); 
-		
-		
+
+
 		JsonArrayRequest platoReq = new JsonArrayRequest(URL_Platos_N, new Response.Listener<JSONArray>() {
-			
+
 			public void onResponse(JSONArray response){
 				Log.d(TAG, response.toString()); 
 				hidePDialog();
-				
+
 				for (int i = 0; i < response.length(); i++) {
 					try {
 						final JSONObject obj = response.getJSONObject(i); 
@@ -97,19 +101,57 @@ public class Act_Menu extends Activity{
 			}
 		});
 		AppController.getInstance().addToRequestQueue(platoReq);
+
+
+		Button but_pagar = (Button)findViewById(R.id.but_finalizPedido); 
+		but_pagar.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent int_pagar = new Intent(getApplicationContext(), Act_RealizarPago.class); 
+				int_pagar.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+				int_pagar.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+				startActivity(int_pagar);
+				finish(); 
+
+			}
+		});
+		/*
+		listView = (ListView)findViewById(R.id.LV_menu); 
+		adapter = new CustomListAdapter(this, listaPlato, new BtnClickListener() {
+
+			@Override
+			public void onBtnClick(int position) {
+
+				Plato plato = new Plato(); 
+				j = plato.getPrecio(); 
+				String k = total.getText().toString(); 
+				if (k != "00") {
+					int l = Integer.parseInt(k); 
+					j = j + l; 
+					total.setText(j);
+				}
+				else{
+					total.setText(k);
+				}
+			}
+		});*/
+
+
+
 	}
 
-@Override
-protected void onDestroy() {
-	super.onDestroy();
-	hidePDialog();
-}
-private void hidePDialog(){
-	if (pDialog != null) {
-		pDialog.dismiss();
-		pDialog = null; 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		hidePDialog();
 	}
-}
+	private void hidePDialog(){
+		if (pDialog != null) {
+			pDialog.dismiss();
+			pDialog = null; 
+		}
+	}
 
 
 }
